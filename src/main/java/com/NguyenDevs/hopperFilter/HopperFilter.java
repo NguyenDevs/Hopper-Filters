d2a4067aae3d164c696611c00f5ba88fbe971325
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-08-09 14:02:25 +0700

    Add default config and call update checker method

--- src/main/java/com/NguyenDevs/hopperFilter/HopperFilter.java
+++ src/main/java/com/NguyenDevs/hopperFilter/HopperFilter.java
@@ -3,6 +3,7 @@ package com.NguyenDevs.hopperFilter;
 import com.NguyenDevs.hopperFilter.Command.HopperCommand;
 import com.NguyenDevs.hopperFilter.Listener.HopperListener;
 import com.NguyenDevs.hopperFilter.Manager.HopperManager;
+import com.NguyenDevs.hopperFilter.Utils.UpdateChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -23,6 +24,7 @@ public class HopperFilter extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         FileConfiguration config = getConfig();
+        config.addDefault("update-notify", true);
         config.addDefault("menu-slot", 27);
         config.addDefault("sound.enabled", true);
         config.addDefault("sound.name", "BLOCK_GRINDSTONE_USE");
@@ -61,6 +63,8 @@ public class HopperFilter extends JavaPlugin {
             saveResource("data.yml", false);
         }
         dataConfig = YamlConfiguration.loadConfiguration(dataFile);
+        UpdateChecker updateChecker = new UpdateChecker(127398, this);
+        updateChecker.checkForUpdate();
 
         hopperManager = new HopperManager(this);
         hopperManager.loadHopperFilters();
@@ -76,7 +80,7 @@ public class HopperFilter extends JavaPlugin {
     @Override
     public void onDisable() {
         hopperManager.saveHopperFilters();
-        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&7HopperFilter&8] &aPlugin disabled!"));
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&7HopperFilter&8] &cPlugin disabled successfully!"));
     }
 
     public FileConfiguration getDataConfig() {
@@ -97,8 +101,37 @@ public class HopperFilter extends JavaPlugin {
 
     public void reloadAllConfigs() {
         reloadConfig();
-        dataConfig = YamlConfiguration.loadConfiguration(dataFile);
+        FileConfiguration config = getConfig();
+        config.addDefault("update-notify", true);
+        config.addDefault("menu-slot", 27);
+        config.addDefault("sound.enabled", true);
+        config.addDefault("sound.name", "BLOCK_GRINDSTONE_USE");
+        config.addDefault("sound.volume", 0.5);
+        config.addDefault("sound.pitch", 1.5);
+        config.addDefault("particle.enabled", true);
+        config.addDefault("particle.name", "FIREWORK");
+        config.addDefault("delay-effect", 10);
+        config.addDefault("disable-world", new String[]{"example", "example_nether", "example_the_end"});
+        config.options().copyDefaults(true);
+        saveConfig();
+
         messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
+        messagesConfig.addDefault("prefix", "&8[&7HopperFilter&8]");
+        messagesConfig.addDefault("filter-gui-title", "&8HopperFilter");
+        messagesConfig.addDefault("no-permission", "&cYou don't have permission to do this!");
+        messagesConfig.addDefault("usage", "&7Usage: /hopperfilter <reload>");
+        messagesConfig.addDefault("reload-success", "&aConfiguration reloaded successfully!");
+        messagesConfig.addDefault("limit-reached", "&cYou have reached your hopper filter limit of %limit%!");
+        messagesConfig.addDefault("world-disabled", "&cThe hopper filter is disabled in this world!");
+        messagesConfig.options().copyDefaults(true);
+
+        try {
+            messagesConfig.save(messagesFile);
+        } catch (IOException e) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&7HopperFilter&8] &cCould not save messages.yml: " + e.getMessage()));
+        }
+
+        dataConfig = YamlConfiguration.loadConfiguration(dataFile);
     }
 
     public HopperManager getHopperManager() {
